46391d248c6fbcfb49fc8ccbcf7a8914348b5353
Varsha Venkatesh <varsha@Varshas-MacBook-Pro.local>
2021-07-04 15:52:18 +0800

    Updated

--- query_final.py
+++ query_final.py
@@ -1,59 +1,64 @@
 import numpy as np 
 import pandas as pd 
-import os
-import re
-import seaborn as sns
+#import os
+#import re
+#import seaborn as sns
 import string
 import nltk
-from nltk.stem.porter import *
+#from nltk.stem.porter import *
 from nltk.tokenize import RegexpTokenizer
+print("before stopwords")
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer 
-from nltk import pos_tag
-from nltk.corpus import wordnet
-import matplotlib.pyplot as plt
-from gensim.parsing.preprocessing import remove_stopwords
-from bs4 import BeautifulSoup 
+print("downloaded stopwords")
+#from nltk.stem import WordNetLemmatizer 
+#from nltk import pos_tag
+#from nltk.corpus import wordnet
+#import matplotlib.pyplot as plt
+#from gensim.parsing.preprocessing import remove_stopwords
 import pandas as pd
-import os
-import numpy as np
-import re
-import numpy as np                                  #for large and multi-dimensional arrays
-import pandas as pd                                 #for data manipulation and analysis
-import nltk                                         #Natural language processing tool-kit
+#import os
+#import numpy as np
+#import re
+#import numpy as np                                  #for large and multi-dimensional arrays
+#import pandas as pd                                 #for data manipulation and analysis
+#import nltk                                         #Natural language processing tool-kit
 import pickle
-from collections import defaultdict
-import copy
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('wordnet')
-from nltk.corpus import stopwords                   #Stopwords corpus
-from nltk.stem import PorterStemmer                 # Stemmer
-from nltk.tokenize import word_tokenize 
-from sklearn.feature_extraction.text import TfidfVectorizer
+#from collections import defaultdict
+#import copy
+#nltk.download('stopwords')
+#nltk.download('punkt')
+#nltk.download('wordnet')
+#from nltk.corpus import stopwords                   #Stopwords corpus
+#from nltk.stem import PorterStemmer                 # Stemmer
+#from nltk.tokenize import word_tokenize 
+#from sklearn.feature_extraction.text import TfidfVectorizer
 
-import re, string, unicodedata                          # Import Regex, string and unicodedata.
+#import re, string, unicodedata                          # Import Regex, string and unicodedata.
                                    # Import contractions library.
-from bs4 import BeautifulSoup   
+#from bs4 import BeautifulSoup   
 from sklearn.feature_extraction.text import CountVectorizer          #For Bag of words
 from sklearn.feature_extraction.text import TfidfVectorizer          #For TF-IDF
-from nltk.tokenize import word_tokenize, sent_tokenize  # Import Tokenizer.
-from nltk.stem.wordnet import WordNetLemmatizer         # Import Lemmatizer.
-nltk.download('averaged_perceptron_tagger')
+#from nltk.tokenize import word_tokenize, sent_tokenize  # Import Tokenizer.
+#from nltk.stem.wordnet import WordNetLemmatizer         # Import Lemmatizer.
+#nltk.download('averaged_perceptron_tagger')
 from sklearn.metrics.pairwise import linear_kernel
-import nltk
-from scipy import sparse
-from scipy import sparse as sp
+#import nltk
+#from scipy import sparse
+#from scipy import sparse as sp
 w_tokenizer = nltk.tokenize.WhitespaceTokenizer()
 lemmatizer = nltk.stem.WordNetLemmatizer()
-data = pd.read_pickle("processed_data_2.pkl")
-df = pd.read_pickle("proc_sentences.pkl")
-tfidf = pickle.load(open("tfidf2.pickle" , "rb"))#TfidfVectorizer().fit_transform(df["Combine"])
 
+data = pd.read_pickle("processed_data_2.pkl")
+print("loaded processed data 2")
+df = pd.read_pickle("proc_sentences.pkl")
+print("loaded proc sentences")
+tfidf = pickle.load(open("tfidf2.pickle" , "rb"))#TfidfVectorizer().fit_transform(df["Combine"])
+print("loaded tfidf")
 queryTFIDF_vector = pickle.load(open("querytfidf2.pickle" , "rb"))#TfidfVectorizer().fit(df["Combine"])
+print("loaded query tfidf")
 tokenizer = RegexpTokenizer(r'\w+')
 stop_words = set(stopwords.words('english'))
-
+print("hello")
 def query_text(query):
 
 	#variables
